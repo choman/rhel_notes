0004af9fe799862e6df3eadd6ae69bb3512d6036
Chad Homan <choman@gmail.com>
2017-08-24 12:40:14 -0500

    basic input arg

--- myinst.sh
+++ myinst.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+args=$*
+
 cd certs
 ./install.sh
 
@@ -7,5 +9,5 @@ cd
 tar xvpf /media/sf_Downloads/rhel_notes/harbor-offline-installer-v1.2.0-rc2.tgz
 cd harbor
 cp /media/sf_Downloads/rhel_notes/files/harbor.cfg .
-sudo ./install.sh --with-clair
+sudo ./install.sh $args
 
