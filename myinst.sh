075400071e2e476e592ee6f5bee2082488807e0f
Chad Homan <choman@gmail.com>
2017-09-11 13:50:31 -0400

    updated version

--- myinst.sh
+++ myinst.sh
@@ -6,7 +6,7 @@ cd certs
 ./install.sh
 
 cd
-tar xvpf /media/sf_Downloads/rhel_notes/harbor-offline-installer-v1.2.0-rc2.tgz
+tar xvpf /media/sf_Downloads/rhel_notes/harbor-offline-installer-v1.2.0.tgz
 cd harbor
 cp /media/sf_Downloads/rhel_notes/files/harbor.cfg .
 sudo ./install.sh $args
