798cf9167f5978c19a84c99de9cbee39ee061f03
Chad Homan <choman@gmail.com>
2017-09-11 13:50:50 -0400

    initial commit

---
+++ certs/remote_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SERVER_IP=172.168.1.40
+SERVER_HN="reg2 reg2.local.net"
+
+echo "$SERVER_IP    $SERVER_HN" | sudo tee -a /etc/hosts
+
+scp dcw_admin@$SERVER_IP:/home/dcw_admin/rhel_notes/certs/local.net.crt .
+
+sudo cp -v local.net.crt /etc/pki/ca-trust/source/anchors/reg2.local.net.crt
+sudo chmod 644 /etc/pki/ca-trust/source/anchors/reg2.local.net.crt
+
+echo "updating trusts"
+sudo update-ca-trust
+
+echo "restarting docker"
+sudo systemctl stop docker
+sudo systemctl start docker
+
